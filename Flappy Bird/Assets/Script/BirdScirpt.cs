bb75f919ee8584f91167f6da04bb0ac822fcdab6
Mücahit Aktürk <m.akturk6134@gmail.com>
2021-02-21 13:52:47 +0300

    Project

---
+++ Flappy Bird/Assets/Script/BirdScirpt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScirpt : MonoBehaviour
+{
+
+    private Rigidbody2D rigidbody2d;
+    private int skor = 0;
+    
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, -45));
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2d.AddForce(new Vector2(0, 200));
+            transform.Rotate(new Vector3(0,0,45));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "TriggerTag")
+        {
+            skor = skor + 1;
+        }
+    }
+}
